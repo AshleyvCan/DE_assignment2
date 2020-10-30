49aee2fc8b043ee76b28fac3ee0d40c1bb4293de
JeroenR123 <jeroenraap@gmail.com>
2020-10-30 20:13:41 +0100

    Weg met die classes

--- streampipeline/pipeline.py
+++ streampipeline/pipeline.py
@@ -172,7 +172,8 @@ def run(argv=None, save_main_session=True):
             subscription=args.subscription)
                 | 'DecodeString' >> beam.Map(lambda b: b.decode('utf-8'))
                 | 'ParsFn' >> beam.Map(parse)
-                | 'Remove_Variance' >> beam.Map(remove_novariance))
+                | 'Remove_Variance' >> beam.Map(remove_novariance)
+                |beam.Map(print))
 
         output = (data | 'Predict' >> beam.ParDo(MyPredictDoFn()))
         output | 'WriteToBQ' >> WriteToBigQuery(
