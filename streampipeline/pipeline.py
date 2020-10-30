82aa07162131f02006d264d37b909050fc32cf36
JeroenR123 <jeroenraap@gmail.com>
2020-10-30 20:08:26 +0100

    Weg met die classes

--- streampipeline/pipeline.py
+++ streampipeline/pipeline.py
@@ -81,6 +81,7 @@ class MyPredictDoFn(beam.DoFn):
     def process(self, element, **kwargs):
         model = joblib.load(beam.io.filesystems.FileSystems.open('gs://de2020labs97/ml_models/model.joblib'))
         df = pd.DataFrame([element])
+        df = df.apply(pd.to_numeric)
         result = model.predict(df)
         results = {'timestamp': df['timestamp'],
                    'RUL': result
@@ -172,7 +173,6 @@ def run(argv=None, save_main_session=True):
                 | 'DecodeString' >> beam.Map(lambda b: b.decode('utf-8'))
                 | 'ParsFn' >> beam.Map(parse)
                 | 'Remove_Variance' >> beam.Map(remove_novariance)
-                | beam.Map(print))
 
         output = (data | 'Predict' >> beam.ParDo(MyPredictDoFn()))
         output | 'WriteToBQ' >> WriteToBigQuery(
