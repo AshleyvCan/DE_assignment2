53843c8906fc73c3ac83a61763bc6bf115e0368d
jeroenR123 <jeroenraap@gmail.com>
2020-11-01 16:20:46 +0100

    Weg met die classes

--- streampipeline/pipeline.py
+++ streampipeline/pipeline.py
@@ -55,7 +55,7 @@ def parse(elem):
             'Sensor_18': [float(row[21])],
             'Sensor_19': [float(row[22])],
             'Sensor_20': [float(row[23])],
-            'timestamp': [int(float(row[24]))/1000],
+            'timestamp': [int(float(row[24])/1000)],
         }
 
 
@@ -178,9 +178,8 @@ def run(argv=None, save_main_session=True):
                 | 'DecodeString' >> beam.Map(lambda b: b.decode('utf-8'))
                 | 'ParsFn' >> beam.Map(parse)
                 | 'Remove_Variance' >> beam.Map(remove_novariance)
-                | 'Predict' >> beam.ParDo(MyPredictDoFn()))
-
-        output = (data | 'WriteToBQ' >> WriteToBigQuery(
+                | 'Predict' >> beam.ParDo(MyPredictDoFn())
+                | 'WriteToBQ' >> WriteToBigQuery(
                             args.table_name,
                             args.dataset,
                             {
